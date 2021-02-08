c8b730e9fe8da7056b81dff06c8d1b9c52466a29
Obada Athamneh <obadaobaida@gmail.com>
2021-02-08 16:21:24 +0200

    This creat by obadao

--- js/app.js
+++ js/app.js
@@ -1,5 +1,54 @@
 'use strict';
 
+let w1 = prompt('do i like red color?')
+w1 = w1.toLocaleLowerCase();
+
+if (w1 === 'yes' || w1 === 'y'){
+    alert(' yes i like red color');
+} else {
+    alert('no i like red color');
+}
+
+
+let w2 = prompt('do i like black color?')
+w2 = w2.toLocaleLowerCase();
+
+if (w2 === 'yes' || w2 === 'y'){
+    alert(' yes i like black color');
+} else {
+    alert('no i like black color');
+}
+
+let w3 = prompt('do i like cake?')
+w3 = w3.toLocaleLowerCase();
+
+if (w3 === 'yes' || w3 === 'y'){
+    alert(' yes i like  cake');
+} else {
+    alert('no i like cake');
+}
+
+let w4 = prompt('do i like shawerma?')
+w4 = w4.toLocaleLowerCase();
+
+if (w4 === 'yes' || w4 === 'y'){
+    alert(' yes i like shawerma very much');
+} else {
+    alert('no i like shawerma');
+}
+
+let w5 = prompt('do i like programming?')
+w5 = w5.toLocaleLowerCase();
+
+if (w5 === 'yes' || w5 === 'y'){
+    alert(' yes i like programming');
+} else {
+    alert('no i like programming');
+}
+
+
+
+
 
 let username = prompt ('what is your name?');
 alert('welcome ' + username);
@@ -15,12 +64,9 @@ console.log(user1.toLocaleLowerCase());
 user1 = user1.toLocaleLowerCase();
 
 if ( user1 === 'obada'){
-    alert('welcome obada');
-    console.log('welcome obada');
-} else if(user1 === 'eshaq') {
-    alert('welcome eshaq');
-    console.log('welcome eshaq');
-} else {
-    alert('welcome mohammad');
-    console.log('welcome mohammad')
+    alert('yes my name is obada');
+    console.log('yes my name is obada');
+}  else {
+    alert('No my name is obada');
+    console.log('no my name is obada')
 }
